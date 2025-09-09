85b23768414f0678d7663057c172636312a2542e
Vinayak Kariappa Chettimada <vich@nordicsemi.no>
2025-09-09 14:06:42 +0200

    soc: gd: add support for gd32c2x1, gd32f527
    Add initial support for the GD32C2x1 and GD32F527 SoC series,
    board, device tree, Kconfig files.
    
    Signed-off-by: Jianping Wu <jianping.wu@gigadevice.com>

---
+++ soc/gd/gd32/gd32f527/soc.h
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2025, GigaDevice Semiconductor Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef ZEPHYR_SOC_GD_GD32_GD32F527_SOC_H_
+#define ZEPHYR_SOC_GD_GD32_GD32F527_SOC_H_
+
+#include <gd32f527.h>
+
+#undef CAN_MODE_NORMAL
+
+#endif /* ZEPHYR_SOC_GD_GD32_GD32F527_SOC_H_ */
